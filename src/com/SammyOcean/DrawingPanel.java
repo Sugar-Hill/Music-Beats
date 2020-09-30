1d60674193bdf5d114f9e97bf255a9e47d19c004
sugar_hill <ayofouad@gmail.com>
2020-09-30 22:11:28 +0100

    Increased shape count

--- src/com/SammyOcean/DrawingPanel.java
+++ src/com/SammyOcean/DrawingPanel.java
@@ -11,27 +11,29 @@ public class DrawingPanel extends JPanel implements ControllerEventListener {
 
     public void controlChange(ShortMessage event) {
         message = true;
-
         repaint();
     }
 
     public void paintComponent(Graphics g) {
         if (message) {
-            Graphics2D graphics2D = (Graphics2D) g;
+            int numberOfShapes = (int) (Math.random() * 20);
 
-            int red = (int) (Math.random() * 250);
-            int green = (int) (Math.random() * 250);
-            int blue = (int) (Math.random() * 250);
+            for (int i = 3; i < numberOfShapes; i++) {
+                Graphics2D graphics2D = (Graphics2D) g;
 
-            graphics2D.setColor(new Color(red, green, blue));
+                int red = (int) (Math.random() * 250);
+                int green = (int) (Math.random() * 250);
+                int blue = (int) (Math.random() * 250);
 
-            int height = (int) ((Math.random() * 120) + 10);
-            int width = (int) ((Math.random() * 120) + 10);
-            int x = (int) ((Math.random() * 40) + 10);
-            int y = (int) ((Math.random() * 40) + 10);
+                graphics2D.setColor(new Color(red, green, blue));
 
-            graphics2D.fill3DRect(x, y, width, height, true);
+                int height = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
+                int x = (int) ((Math.random() * 150) + 10);
+                int y = (int) ((Math.random() * 150) + 10);
 
+                graphics2D.fill3DRect(x, y, width, height, true);
+            }
             message = false;
         }
     }
